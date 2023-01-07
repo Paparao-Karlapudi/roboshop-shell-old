740a810136ee136926ba6851c82d540250d1ea18
pappik <pappik@gmail.com>
2023-01-06 23:53:50 -0500

    variable and if testing

--- frontend.sh
+++ frontend.sh
@@ -6,5 +6,5 @@ rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-cp ./files/frontend-roboshop.config /etc/nginx/default.d/roboshop.config
+cp "$script_location"/files/frontend-roboshop.config /etc/nginx/default.d/roboshop.config
 systemctl restart nginx

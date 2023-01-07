f7b696b67971c040c64f0ff80e6c7d88effc90ef
pappik <pappik@gmail.com>
2023-01-06 23:50:07 -0500

    variable and if testing

--- frontend.sh
+++ frontend.sh
@@ -1,4 +1,4 @@
-script_location= $(pwd)
+script_location=$(pwd)
 yum install nginx -y
 systemctl enable nginx
 systemctl start nginx
@@ -6,5 +6,5 @@ rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-cp ${script_location}/files/nginx-roboshop-config.sh /etc/nginx/default.d/roboshop.config
+cp ${script_location}/files/frontend-roboshop.config /etc/nginx/default.d/roboshop.config
 systemctl restart nginx

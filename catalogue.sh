d015e1b326c80a8b6eb8301f56035d1bc0d688d7
pappik <pappik@gmail.com>
2023-01-10 14:16:47 -0500

    catalogue service

--- catalogue.sh
+++ catalogue.sh
@@ -5,6 +5,7 @@ yum install nodejs -y
 useradd roboshop
 mkdir /app
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+rm -rf /app/*
 cd /app
 unzip /tmp/catalogue.zip
 npm install
--- create-server.sh
+++ create-server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ##### Change these values ###
-ZONE_ID="Z100843328BJM5H0SW95"
-DOMAIN="pappikdev.in"
+ZONE_ID="Z0366464237Z7LZLZPKFA"
+DOMAIN="devopsb70.online"
 SG_NAME="allow-all"
 env=dev
 #############################

82554bab8054ea1547416aa63d0f026037b1532b
pappik <pappik@gmail.com>
2023-01-17 12:34:04 -0500

    improved frontend

--- frontend.sh
+++ frontend.sh
@@ -3,7 +3,8 @@ LOG=/tmp/roboshop.log
 
 echo -e "\e[35m Install Nginx\e[0m"
 yum install nginx -y &>>${LOG}
-if[ $? -eq 0 ]; then
+if[ $? -eq 0 ]
+then
   echo SUCCESS
 else
   echo FAILURE
@@ -11,7 +12,8 @@ fi
 
 echo -e "\e[35m Remove Nginx Old content \e[0m"
 rm -rf /usr/share/nginx/html/* &>>${LOG}
-if[ $? -eq 0 ]; then
+if[ $? -eq 0 ]
+then
   echo SUCCESS
 else
   echo FAILURE
@@ -19,7 +21,8 @@ fi
 
 echo -e "\e[33m Download Frontend Content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
-if[ $? -eq 0 ]; then
+if[ $? -eq 0 ]
+then
   echo SUCCESS
 else
   echo FAILURE
@@ -29,7 +32,8 @@ cd /usr/share/nginx/html
 
 echo -e "\e[35m Extract Frontend Content\e[0m"
 unzip /tmp/frontend.zip &>>${LOG}
-if[ $? -eq 0 ]; then
+if[ $? -eq 0 ]
+then
   echo SUCCESS
 else
   echo FAILURE
@@ -37,7 +41,8 @@ fi
 
 echo -e "\e[35m Copy config files\e[0m"
 cp "$script_location"/files/frontend-roboshop /etc/nginx/default.d/roboshop.conf
-if[ $? -eq 0 ]; then
+if[ $? -eq 0 ]
+then
   echo SUCCESS
 else
   echo FAILURE
@@ -45,7 +50,8 @@ fi
 
 echo -e "\e[35m Enable Nginx\e[0m"
 systemctl enable nginx &>>${LOG}
-if[ $? -eq 0 ]; then
+if[ $? -eq 0 ]
+then
   echo SUCCESS
 else
   echo FAILURE
@@ -53,7 +59,8 @@ fi
 
 echo -e "\e[35m Restart Nginx\e[0m"
 systemctl restart nginx &>>${LOG}
-if[ $? -eq 0 ]; then
+if[ $? -eq 0 ]
+then
   echo SUCCESS
 else
   echo FAILURE

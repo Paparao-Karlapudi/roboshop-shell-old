5f2f082cc2b35e4d29be4073d023d3a45142eb26
pappik <pappik@gmail.com>
2023-01-17 11:46:25 -0500

    improved frontend

--- frontend.sh
+++ frontend.sh
@@ -1,11 +1,29 @@
 script_location=$(pwd)
-yum install nginx -y
-systemctl enable nginx
-systemctl start nginx
-rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+LOG=/tmp/roboshop.log
+
+echo -e "\e[35m Install Nginx\e[0m"
+yum install nginx -y &>>${LOG}
+echo $?
+
+echo -e "\e[35m Remove Nginx Old content \e[0m"
+rm -rf /usr/share/nginx/html/* &>>${LOG}
+echo &?
+
+echo -e "\e[Download Frontend Content\e[0m"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
+eco &?
+
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+
+echo -e "\e[35m Extract Frontend Content\e[0m"
+unzip /tmp/frontend.zip &>>${LOG}
+echo &?
+
+echo -e "\e[35m Copy config files\e[0m"
 cp "$script_location"/files/frontend-roboshop /etc/nginx/default.d/roboshop.conf
 
-systemctl restart nginx
+echo -e "\e[35m Enable Nginx\e[0m"
+systemctl enable nginx &>>${LOG}
+
+echo -e "\e[35m Restart Nginx\e[0m"
+systemctl restart nginx &>>${LOG}

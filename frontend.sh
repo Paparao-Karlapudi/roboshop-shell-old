8b0cd1cd1aaeb3cc2ca89b31b40e3a8ddda3225b
pappik <pappik@gmail.com>
2023-01-22 00:15:59 -0500

    improved frontend & catalogue. Adding common.sh

--- frontend.sh
+++ frontend.sh
@@ -7,23 +7,23 @@ echo -e "\e[35m Remove Nginx Old content \e[0m"
 rm -rf /usr/share/nginx/html/* &>>${LOG}
 status_check()
 
-echo -e "\e[33m Download Frontend Content\e[0m"
+echo -e "\e[35m Download Frontend Content \e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
 status_check()
 cd /usr/share/nginx/html
 
-echo -e "\e[35m Extract Frontend Content\e[0m"
+echo -e "\e[35m Extract Frontend Content \e[0m"
 unzip /tmp/frontend.zip &>>${LOG}
 status_check()
 
-echo -e "\e[35m Copy config files\e[0m"
+echo -e "\e[35m Copy config files \e[0m"
 cp "$script_location"/files/frontend-roboshop /etc/nginx/default.d/roboshop.conf
 status_check()
 
-echo -e "\e[35m Enable Nginx\e[0m"
+echo -e "\e[35m Enable Nginx \e[0m"
 systemctl enable nginx &>>${LOG}
 status_check()
 
-echo -e "\e[35m Restart Nginx\e[0m"
+echo -e "\e[35m Restart Nginx \e[0m"
 systemctl restart nginx &>>${LOG}
 status_check()

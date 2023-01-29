a21728881e9e7408c89303b101982372ae7adea0
pappik <pappik@gmail.com>
2023-01-29 16:02:45 -0500

    mysql

--- mysql.sh
+++ mysql.sh
@@ -1,25 +1,25 @@
 source common.sh
-if [ -z "root_mysql_password"]; then
+if [ -z "root_mysql_password" ]; then
   echo "variable roo_mysql_password is missing"
 fi
 
-print_head "Disable mySQL default module"
+print "Disable mySQL default module"
 dnf module disable mysql -y &>>${LOG}
 status_check
 
-print_head "Copy mysql repo file"
+print"Copy mysql repo file"
 cp ${script_location}/files/mysql.repo /etc/yum.repos.d/myswl.repo &>>${LOG}
 status_check
 
-print_head "Install MySQL server"
+print "Install MySQL server"
 yum install mysql-community-server -y &>>${LOG}
 
-print_head "Enable Mysql"
+print"Enable Mysql"
 systemctl enable mysqld &>>${LOG}
 
-print_head "start Mysql"
+print "start Mysql"
 systemctl start mysqld &>>${LOG}
 
-print_head "Reset default database password"
+print "Reset default database password"
 mysql_secure_installation --set-root-pass RoboShop@1 &>>${LOG}
 
